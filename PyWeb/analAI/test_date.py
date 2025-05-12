078d6df657c04bae78cdbe1c4842226742a39a44
dmsgur <dmsgur@gmail.com>
2025-05-12 15:27:59 +0900

    1

---
+++ PyWeb/analAI/test_date.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+#yyyy-MM-dd HH:mm:ss
+curdatetime =datetime.now().replace(microsecond=0)
+print(curdatetime)#현재날짜
+pre_datetime=curdatetime-timedelta(days=200)
+print(pre_datetime)
+ppre_datetime=pre_datetime-timedelta(hours=240)
+print(ppre_datetime)
+pppre_datetime=ppre_datetime-timedelta(minutes=1*10)
+print(pppre_datetime)
+pre_week = pppre_datetime-relativedelta(weeks=1)
+print(pre_week)
+pre_month = pre_week-relativedelta(months=1)
+print(pre_month)
+
+
--- PyWeb/analAI/train_model.py
+++ PyWeb/analAI/train_model.py
@@ -1,5 +1,65 @@
+# py version 3.9
+import sys
 import tensorflow as tf # tensorflow-cpu 2.10.0
 import numpy as np # numpy 1.26.4
+import requests
 import matplotlib.pyplot as plt
 from tensorflow.keras import Sequential,Model,Input
 from tensorflow.keras.layers import Dense,LSTM,ConvLSTM1D
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+# == 동일 버전 ==
+print("python -v ",sys.version)
+print("tensorflow-cpu -v",tf.__version__)
+print("numpy -v ",np.__version__)
+NAME_URL = r"https://api.bithumb.com/v1/market/all"
+MAIN_URL = r"https://api.bithumb.com/v1/candles/"
+# https://api.bithumb.com/v1/candles/minutes/{unit}
+# https://api.bithumb.com/v1/candles/days
+# https: // api.bithumb.com / v1 / candles / weeks
+# https://api.bithumb.com/v1/candles/months
+resobj = requests.get(NAME_URL).json()# ret [ {dict} ]
+#[{'market': 'KRW-BTC', 'korean_name': '비트코인', 'english_name': 'Bitcoin'}, {'market': 'KRW-ETH', 'korea
+# => 변경 {BTC:["Bitcoin":"비트코인"],ETC:["ether..":"이더리움"]}
+names={}
+for unit in resobj:
+    n = unit["market"].split("-")[1]
+    names[n]=[unit["english_name"],unit["korean_name"]]
+print(names)
+def receive_data(target_name="BTC",req_time="days",getcnt=200):
+    dt=None
+    data_sets=[]# 최근데이터를 맨 뒤로 보냄
+    date_datas = []# 수신된 날짜 데이터
+    minutetime =0
+    if type(req_time)==int:
+        minutetime=req_time
+        req_time="minutes/"+str(req_time)
+        curdatetime = datetime.now().replace(microsecond=0)
+        dt = curdatetime
+    while(True):
+        #yyyy-MM-dd HH:mm:ss
+        params = {"market":"KRW-"+target_name,"to":dt,"count":getcnt}
+        result = requests.get(MAIN_URL+req_time,params)
+        res = result.json()
+        res.reverse()
+        data_sets.extend(\
+            [o for o in res if o["candle_date_time_kst"] in date_datas])
+        #minutes , days, weeks, months
+        if req_time=="days":
+            dt = dt-timedelta(days=getcnt)
+        elif req_time=="months":
+            dt = dt - relativedelta(months=getcnt)
+        elif req_time=="weeks":
+            dt = dt - relativedelta(weeks=getcnt)
+        else:
+            dt = dt - timedelta(minutes=getcnt*minutetime)
+        date_datas.extend([ o["candle_date_time_kst"] for o in res ])
+        if len(data_sets)>=getcnt:break
+    return data_sets
+    # requests.get()
+    #pass #주소로부터 데이터 수신
+
+if "__main__"==__name__:
+    # months, weeks,days, minutes 분 단위 : 1, 3, 5, 10, 15, 30, 60, 240
+    #receive_data()
+    receive_data(req_time=1)
