77c76187598cc32a3b7d2f5fb44b5a383dbe1f35
dmsgur <dmsgur@gmail.com>
2025-05-12 13:30:15 +0900

    1

--- PyWeb/analAI/train_model.py
+++ PyWeb/analAI/train_model.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-import numpy as np
+import tensorflow as tf # tensorflow-cpu 2.10.0
+import numpy as np # numpy 1.26.4
 import matplotlib.pyplot as plt
 from tensorflow.keras import Sequential,Model,Input
 from tensorflow.keras.layers import Dense,LSTM,ConvLSTM1D
