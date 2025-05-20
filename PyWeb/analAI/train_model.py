569bb103367f9499ff1df3a8aeb368499d1a7574
dmsgur <dmsgur@gmail.com>
2025-05-20 11:29:59 +0900

    1

--- PyWeb/analAI/train_model.py
+++ PyWeb/analAI/train_model.py
@@ -363,29 +363,29 @@ class UserService():
             ret_dict["info"]={"coinname":coinname,"timestepstr":timestepstr,"req_time":req_time,"model_type":train_type}
             ret_text+="1. 시작가 비교 --------------------------------------------------------\n"
             ret_text+=f"opening_price pred:{rec_pred[0][0]:.4f} recent err rate:{pred_avgrat[0]:.2%}\n"
-            ret_text+=f"실제값 {y_data[-1][0]}, 예측값 {y_pred[-1][0]}\n"
+            ret_text+=f"실제값 {y_raw[-1][0]}, 예측값 {y_pred[-1][0]}\n"
             ret_dict["open_pri"] = {"cur_pred":round(rec_pred[0][0],4),"errrat":round(pred_avgrat[0],2),
-                                    "pre_true":y_data[-1][0],"pre_pred":y_pred[-1][0]}
+                                    "pre_true":y_raw[-1][0],"pre_pred":y_pred[-1][0]}
             ret_text+="2. 최고가 비교 --------------------------------------------------------\n"
             ret_text+=f"high_price pred:{rec_pred[0][1]:.4f} recent err rate:{pred_avgrat[1]:.2%}\n"
-            ret_text+=f"실제값 {y_data[-1][1]}, 예측값 {y_pred[-1][1]}\n"
+            ret_text+=f"실제값 {y_raw[-1][1]}, 예측값 {y_pred[-1][1]}\n"
             ret_dict["high_pri"] = {"cur_pred": round(rec_pred[0][1], 4), "errrat": round(pred_avgrat[1], 2),
-                                    "pre_true": y_data[-1][1], "pre_pred": y_pred[-1][1]}
+                                    "pre_true": y_raw[-1][1], "pre_pred": y_pred[-1][1]}
             ret_text+="3. 최저가 비교 --------------------------------------------------------\n"
             ret_text+=f"low_price pred:{rec_pred[0][2]:.4f} recent err rate:{pred_avgrat[2]:.2%}\n"
-            ret_text+=f"실제값 {y_data[-1][2]}, 예측값 {y_pred[-1][2]}\n"
+            ret_text+=f"실제값 {y_raw[-1][2]}, 예측값 {y_pred[-1][2]}\n"
             ret_dict["low_pri"] = {"cur_pred": round(rec_pred[0][2], 4), "errrat": round(pred_avgrat[2], 2),
-                                    "pre_true": y_data[-1][2], "pre_pred": y_pred[-1][2]}
+                                    "pre_true": y_raw[-1][2], "pre_pred": y_pred[-1][2]}
             ret_text+="4. 가격 총 거래량 비교 --------------------------------------------------\n"
             ret_text+=f"candle_acc_trade_price pred:{rec_pred[0][3]:.4f} recent err rate:{pred_avgrat[3]:.2%}\n"
-            ret_text+=f"실제값 {y_data[-1][3]}, 예측값 {y_pred[-1][3]}\n"
+            ret_text+=f"실제값 {y_raw[-1][3]}, 예측값 {y_pred[-1][3]}\n"
             ret_dict["tot_pri"] = {"cur_pred": round(rec_pred[0][3], 4), "errrat": round(pred_avgrat[3], 2),
-                                   "pre_true": y_data[-1][3], "pre_pred": y_pred[-1][3]}
+                                   "pre_true": y_raw[-1][3], "pre_pred": y_pred[-1][3]}
             ret_text+="5. 현재가 비교 --------------------------------------------------------\n"
             ret_text+=f"trade_price pred:{rec_pred[0][4]:.4f} recent err rate:{pred_avgrat[4]:.2%}\n"
-            ret_text+=f"실제값 {y_data[-1][4]}, 예측값 {y_pred[-1][4]}\n"
+            ret_text+=f"실제값 {y_raw[-1][4]}, 예측값 {y_pred[-1][4]}\n"
             ret_dict["cur_pri"] = {"cur_pred": round(rec_pred[0][4], 4), "errrat": round(pred_avgrat[4], 2),
-                                   "pre_true": y_data[-1][4], "pre_pred": y_pred[-1][4]}
+                                   "pre_true": y_raw[-1][4], "pre_pred": y_pred[-1][4]}
         return ret_text,ret_dict;
 def web_service(coinname,timestep_str,modeltype,req_time):#coinname 이름 timestep_str="middle",
     # 4. ======== 사용자 예측값 출력
@@ -468,26 +468,26 @@ if "__main__"==__name__:
     #print(res_text)
     #{'BTC': ['Bitcoin', '비트코인'], 'ETH': ['Ethereum', '이더리움'], 'ETC': ['Ethereum Classic', '이더리움 클래식'], 'XRP': ['XR
     # # 최초 모델 훈련 자동
-    print(names.keys())
-    #names_arr = [list(names.keys())]
-    names_arr=["BTC", "ETH", "XRP"]
-    GETCNT = 400
-    time_steps = ["middle","long"]
-    # req_times = [10, 30, 60, 240,"days","weeks","months"]
-    req_times = [60,"days"]
-    progressval = len(time_steps)*len(req_times)*len(names_arr)*len(["lstm","conv"])
-    progresscnt=0
-    for cname in ["BTC","ETH","XRP"]:
-        for time_step in time_steps:
-            for req in req_times:
-                progresscnt+=1
-                print(f"xxxxxxxxxxxxxxxxxxxxxxxxxx 현재 진행율 :{progresscnt//progressval:%} {progresscnt}/{progressval}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-                # middle 60 (conv lstm)
-                if req == "months" and time_step == "llong": continue
-                t_admin = ConfingData(coinname=cname, timestepstr=time_step, req_time=req)
-                t_models = {"conv":createModel_conv(time_step), "lstm":createModel_lstm(time_step)}
-                tcbs = createCallback(cname)
-                t_admin.init_train(smodels=t_models, cbs=tcbs, epoch=20, batsize=None)
+    # print(names.keys())
+    # #names_arr = [list(names.keys())]
+    # names_arr=["BTC", "ETH", "XRP"]
+    # GETCNT = 400
+    # time_steps = ["middle","long"]
+    # # req_times = [10, 30, 60, 240,"days","weeks","months"]
+    # req_times = [60,"days"]
+    # progressval = len(time_steps)*len(req_times)*len(names_arr)*len(["lstm","conv"])
+    # progresscnt=0
+    # for cname in ["XRP"]:
+    #     for time_step in time_steps:
+    #         for req in req_times:
+    #             progresscnt+=1
+    #             print(f"xxxxxxxxxxxxxxxxxxxxxxxxxx 현재 진행율 :{progresscnt/progressval*100:%} {progresscnt}/{progressval}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    #             # middle 60 (conv lstm)
+    #             if req == "months" and time_step == "llong": continue
+    #             t_admin = ConfingData(coinname=cname, timestepstr=time_step, req_time=req)
+    #             t_models = {"conv":createModel_conv(time_step), "lstm":createModel_lstm(time_step)}
+    #             tcbs = createCallback(cname)
+    #             t_admin.init_train(smodels=t_models, cbs=tcbs, epoch=20, batsize=None)
 
      # #업그레이드 모델 훈련 자동황
      #    # print(names.keys())
